5eb4747abc5ece9f3d6b7065c1cb4906a8587acc
GregSCooper2514 <kirill.shestov123@gmail.com>
2024-10-23 22:30:37 +0300

    Crawling successfully completed

--- test_example.py
+++ test_example.py
@@ -3,21 +3,25 @@ from playwright.sync_api import Page
 from time import sleep
 
 
-def test_example(page: Page) -> None: 
-    company = "Deutsche Bank"
-    location = "Greater Barcelona Metropolitan Area"
+def test_example(page: Page) -> None:
+    email = "meshareef19@gmail.com"
+    password = ".-n76b*U8^vMRn_"
+    company = "Deutsche Bahn"
+    location = "Frankfurt Rhine-Main Metropolitan Area"
     jobTile = "HR"
     messageCount = 0
     connectCount = 0
     followCount = 0
+    pendingCount = 0
+    linkedInMemberCount = 0
     # location = input("Enter the location(exactly how on the web site): ")
     # jobTile = input("Enter the job title: ")
     # company = input("Enter the company name: ")
     page.goto("https://www.linkedin.com/")
     page.locator("[data-test-id=\"home-hero-sign-in-cta\"]").click()
-    page.get_by_label("Email or phone").fill("meshareef19@gmail.com")
+    page.get_by_label("Email or phone").fill(email)
     page.get_by_label("Password").click()
-    page.get_by_label("Password").fill(".-n76b*U8^vMRn_")
+    page.get_by_label("Password").fill(password)
     page.get_by_label("Sign in", exact=True).click()
     page.get_by_placeholder("Search", exact=True).click()
     page.get_by_placeholder("Search", exact=True).fill(company)
@@ -34,20 +38,61 @@ def test_example(page: Page) -> None:
     page.get_by_role("combobox", name="Add a location").press("ArrowDown")
     page.get_by_role("combobox", name="Add a location").press("Enter")
     page.get_by_label("Apply current filters to show").click()
-    results_text = page.get_by_text(re.compile(r"\d+ results")).inner_text()
-    results_number = int(re.search(r"\d+", results_text).group())
-    loops = 1  # Set the number of loops from the number of people
-    for a in range(loops):
-        page.keyboard.press("Tab")
-        name = page.evaluate("document.activeElement.innerText")
-        if name == "Try Premium Free For 1 Month":
+    results_text = page.locator("div:not([class]):not([id])", has_text=re.compile(r"\d+ results")).inner_text()
+    results_number = int(re.search(r'\d+', results_text).group())
+    numberOfPages = results_number // 10
+    remainder = results_number % 10
+    page.keyboard.press("Tab")
+    for a in range(numberOfPages):
+        for b in range(10):
             page.keyboard.press("Tab")
+            sleep(0.4)
             name = page.evaluate("document.activeElement.innerText")
+            if name == "LinkedIn Member":
+                linkedInMemberCount += 1
+                continue
+            link = page.evaluate("document.activeElement.href")
+            page.keyboard.press("Tab")
+            sleep(0.4)
+            button = page.locator("document.activeElement")
+            button_text = page.evaluate("document.activeElement.innerText")
+            if button_text not in ["Message", "Connect", "Follow", "Pending"]:
+                page.keyboard.press("Tab")
+                sleep(0.4)
+                button = page.locator("document.activeElement")
+                button_text = page.evaluate("document.activeElement.innerText")
+            if button_text == "Message":
+                messageCount += 1
+                pass  # Nothing to do here
+            elif button_text == "Connect":
+                connectCount += 1
+                # button.click()
+                # page.get_by_text("Connect").click()
+                # page.get_by_text("Send without a note").click()
+                pass
+            elif button_text == "Follow":
+                followCount += 1
+                pass  # Handle later
+            elif button_text == "Pending":
+                pendingCount += 1
+                pass  # Nothing to do here
+            sleep(0.5)
+        page.get_by_label("Next").click()
+        sleep(2)
+    for a in range(remainder):
+        page.keyboard.press("Tab")
+        sleep(0.4)
+        name = page.evaluate("document.activeElement.innerText")
         link = page.evaluate("document.activeElement.href")
         page.keyboard.press("Tab")
+        sleep(0.4)
         button = page.locator("document.activeElement")
         button_text = page.evaluate("document.activeElement.innerText")
-        print(name, link, button_text)
+        if button_text == "":
+            page.keyboard.press("Tab")
+            sleep(0.4)
+            button = page.locator("document.activeElement")
+            button_text = page.evaluate("document.activeElement.innerText")
         if button_text == "Message":
             messageCount += 1
             pass  # Nothing to do here
@@ -60,3 +105,11 @@ def test_example(page: Page) -> None:
         elif button_text == "Follow":
             followCount += 1
             pass  # Handle later
+        elif button_text == "Pending":
+            pendingCount += 1
+            pass  # Nothing to do here
+        sleep(0.5)
+    print("Message: ", messageCount)
+    print("Connect: ", connectCount)
+    print("Follow: ", followCount)
+    print("Pending: ", pendingCount)
